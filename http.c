5fbfb9ed8c35b6ae703ca69a7cd603908245d001
Bilal Brarou <braroubilal@gmail.com>
2025-06-20 08:41:34 +0200

    Removing useless Windows checks

--- http.c
+++ http.c
@@ -186,9 +186,6 @@ int main( int argc, char *argv[ ] )
   struct hostent *host_name;
   struct sockaddr_in host_address;
   int ind;
-#ifdef WIN32
-  WSADATA wsaData;
-#endif
 
   if ( argc < 2 )
   {
@@ -218,14 +215,6 @@ int main( int argc, char *argv[ ] )
   }
 
   printf( "Connecting to host '%s'\n", host );
-  // Step 1: open a socket connection on http port with the destination host.
-#ifdef WIN32
-  if ( WSAStartup( MAKEWORD( 2, 2 ), &wsaData ) != NO_ERROR )
-  {
-    fprintf( stderr, "Error, unable to initialize winsock.\n" );
-    return 2;
-  }
-#endif
 
   client_connection = socket( PF_INET, SOCK_STREAM, 0 );
 
@@ -271,19 +260,12 @@ int main( int argc, char *argv[ ] )
 
   printf( "Shutting down.\n" );
 
-#ifdef WIN32
-  if ( closesocket( client_connection ) == -1 )
-#else
+
   if ( close( client_connection ) == -1 )
-#endif
   {
     perror( "Error closing client connection" );
     return 5;
   }
-
-#ifdef WIN32
-  WSACleanup();
-#endif
 
   return 0;
 }
