65f050f76e8f093d968156afdc79b2cc029c5058
Bilal Brarou <braroubilal@gmail.com>
2025-06-20 20:27:11 +0200

    get, set, clear done

--- des.c
+++ des.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
 #include <stdbool.h>
-#define GET_BIT(array,bit)
-#define SET_BIT(array,bit)
-#define CLR_BIT(array,bit)
+#define GET_BIT(array,bit) array[(int) array[bit/8]] & (0x80 >> (bit % 8))
+#define SET_BIT(array,bit) array[(int) array[bit/8]] |= (0x80 >> (bit % 8))
+#define CLR_BIT(array,bit) array[(int) array[bit/8]] &= ~(0x80 >> (bit % 8))
 
 
 bool get_bit(char * array, int bit) {
