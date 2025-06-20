34ad1fe1a064a8a0d15fc127acde165f4fb1d0b2
Bilal Brarou <braroubilal@gmail.com>
2025-06-20 20:20:23 +0200

    function testing

--- des.c
+++ des.c
@@ -1,4 +1,13 @@
-
+#include <stdio.h>
+#include <stdbool.h>
 #define GET_BIT(array,bit)
 #define SET_BIT(array,bit)
 #define CLR_BIT(array,bit)
+
+
+bool get_bit(char * array, int bit) {
+    return array[(int) array[bit/8]] & (0x80 >> (bit % 8)); //big endian
+}
+int main() {
+
+}
