4ce51556fd0d32433e2b2c389b1d88d4b81ca6bd
Bilal Brarou <braroubilal@gmail.com>
2025-06-20 22:08:13 +0200

    very simple xoring func

--- des.c
+++ des.c
@@ -1,12 +1,15 @@
-#include <stdbool.h>
 #define GET_BIT(array,bit) array[(int) array[bit/8]] & (0x80 >> (bit % 8))
 #define SET_BIT(array,bit) array[(int) array[bit/8]] |= (0x80 >> (bit % 8))
 #define CLR_BIT(array,bit) array[(int) array[bit/8]] &= ~(0x80 >> (bit % 8))
 
+static void xor(unsigned char * target, const unsigned char * src, int len){
+    while(len--) {
+        target[len] ^= src[len];
+    }
 
-bool get_bit(char * array, int bit) {
-    return array[(int) array[bit/8]] & (0x80 >> (bit % 8)); //big endian
 }
+
+
 int main() {
 
 }
